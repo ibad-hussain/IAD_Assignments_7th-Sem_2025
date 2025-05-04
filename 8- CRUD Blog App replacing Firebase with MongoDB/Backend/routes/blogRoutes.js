fe3277a5c00728489a883f5689d17dc4923ebbc5
ibad-hussain <ibadhussain2212@gmail.com>
2025-05-04 22:55:08 +0500

    Removed unnecessary files

--- 8- CRUD Blog App replacing Firebase with MongoDB/Backend/routes/blogRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getAllBlogs, createBlog } from "../controllers/blogController.js";
-
-const blogRouter = express.Router();
-
-blogRouter.get('/all-blogs', getAllBlogs);
-blogRouter.post('/create-blog', createBlog);
-
-export default blogRouter;
